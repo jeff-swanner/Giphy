3462fa79f73fe6a998dc8c14b35b7d8108d49547
Jeff Swanner <jeffswanner93@gmail.com>
2019-08-05 07:44:00 -0400

    Changed to sports theme, buttons now search, add a sport button now works

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,18 +1,40 @@
 $(document).ready(function(){
     var topics = {
-        animals: ["tiger","bear","lion"]
+        animals: ["golf","basketball","football","mma","soccer","tennis","lacross","hockey","baseball","rugby","table tennis","volleyball","field hockey","cricket","formula 1","volleyball","snowboarding","bowling"]
     };
+    function bubble_Sort(a) {
+        var swapp;
+        var n = a.length-1;
+        var x=a;
+        do {
+            swapp = false;
+            for (var i=0; i < n; i++) {
+                if (x[i] > x[i+1]) {
+                   var temp = x[i];
+                   x[i] = x[i+1];
+                   x[i+1] = temp;
+                   swapp = true;
+                };
+            };
+            n--;
+        } while (swapp);
+     return x; 
+    };
+    topics.animals = bubble_Sort(topics.animals);
 
     console.log(topics.animals);
-
-    for (i=0;i<topics.animals.length;i++) {
-        var newButton = $("<button>")
-        newButton.text(topics.animals[i]);
-        newButton.attr("class","giphySearch")
-        $("#buttons").append(newButton);
+    function buttonGen() {
+        $("#buttons").empty();
+        for (i=0;i<topics.animals.length;i++) {
+            var newButton = $("<button>")
+            newButton.text(topics.animals[i]);
+            newButton.attr("class","giphySearch btn btn-outline-info")
+            $("#buttons").append(newButton);
+        };
     };
+    buttonGen();
 
-    $(".giphySearch").click(function(){
+    $(document).on('click','.giphySearch',function(){
         var queryURL = "https://api.giphy.com/v1/gifs/search?q="+$(this).text()+"&limit=10&api_key=Lk61WOxNUNNqUE4NAgrAeLrACvNBXyaz";
             $.ajax({
                 url: queryURL,
@@ -21,7 +43,8 @@ $(document).ready(function(){
                 console.log(response);
                 $("#gifs").empty();
                 for (i=0;i<response.data.length;i++) {
-                    var newDiv = $("<div>")
+                    var newDiv = $("<div>");
+                    newDiv.attr("class","col-lg-4")
                     newDiv.html("<p>Rating: "+response.data[i].rating+"</p>");
                     var imageURL = response.data[i].images.fixed_width.url;
                     newImg = $("<img>");
@@ -31,5 +54,13 @@ $(document).ready(function(){
                     $("#gifs").append(newDiv);
                 };
             });
+    });
+    $("#searchSubmit").click(function(event) {
+        event.preventDefault();
+        let input = $("#searchTerm").val();
+        $("#searchTerm").val("");
+        topics.animals.push(input);
+        topics.animals = bubble_Sort(topics.animals);
+        buttonGen();
     });
 });
